6deb79f4ae7fd5280238899488149e93db11ff38
lincolnrelias <lincolnreistdk@gmail.com>
2024-09-08 18:34:24 -0300

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,6 +15,6 @@ fun main() {
     // Since the tester restarts your program quite often, setting SO_REUSEADDR
     // ensures that we don't run into 'Address already in use' errors
     serverSocket.reuseAddress = true
-    val response = if (input.readLine().split("/")[1].length == 5) "HTTP/1.1 200 OK\r\n\r\n" else "HTTP/1.1 404 Not Found\\r\\n\\r\\n"
+    val response = if (input.readLine().split("/")[1].length == 5) "HTTP/1.1 200 OK\r\n\r\n" else "HTTP/1.1 404 Not Found\r\n\r\n"
     output.println(response)
 }
