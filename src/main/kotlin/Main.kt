d144d06f19585c7268912bea3fe995da9443fce4
lincolnrelias <lincolnreistdk@gmail.com>
2024-09-08 19:32:58 -0300

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,19 +2,31 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.ServerSocket;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress
+
 fun main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println("Logs from your program will appear here!")
-    
+
     // Uncomment this block to pass the first stage
-    var serverSocket = ServerSocket(4221)
-    val clientSocket = serverSocket.accept()
-    println("accepted new connection")
-    val input = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
-    val output = PrintWriter(clientSocket.getOutputStream(), true)
-    // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    // ensures that we don't run into 'Address already in use' errors
-    serverSocket.reuseAddress = true
-    val response = if (input.readLine().split("/")[1].length == 5) "HTTP/1.1 200 OK\r\n\r\n" else "HTTP/1.1 404 Not Found\r\n\r\n"
-    output.println(response)
+    val serverSocket = ServerSocket( 4221)
+        val clientSocket = serverSocket.accept()
+        println("accepted new connection")
+        val input = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+        val output = PrintWriter(clientSocket.getOutputStream(), true)
+        // Since the tester restarts your program quite often, setting SO_REUSEADDR
+        // ensures that we don't run into 'Address already in use' errors
+        serverSocket.reuseAddress = true
+        val splitResponse = input.readLine().split("/")
+        var response ="HTTP/1.1 200 OK\r\n"
+
+        if(splitResponse.size>3){
+            if(splitResponse[1] == "echo"){
+                response+="Content-Type: text/plain\r\nContent-Length: ${splitResponse[2].length}\r\n\r\n${splitResponse[2].split(" ")[0]}"
+            }
+        }
+        output.println(response)
+        input.close()
+        output.close()
+        clientSocket.close()
+
+
 }
