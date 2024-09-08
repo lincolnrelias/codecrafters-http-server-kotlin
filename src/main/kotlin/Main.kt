3d03f32b6394f3c74e7a017d6517ee6348050697
lincolnrelias <lincolnreistdk@gmail.com>
2024-09-08 15:35:01 -0300

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.net.ServerSocket;
 import java.io.PrintWriter;
 fun main() {
@@ -8,10 +10,11 @@ fun main() {
     var serverSocket = ServerSocket(4221)
     val clientSocket = serverSocket.accept()
     println("accepted new connection")
+    val input = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
     val output = PrintWriter(clientSocket.getOutputStream(), true)
     // Since the tester restarts your program quite often, setting SO_REUSEADDR
     // ensures that we don't run into 'Address already in use' errors
     serverSocket.reuseAddress = true
-    val response = "HTTP/1.1 200 OK\r\n\r\n"
+    val response = if (input.readLine().split("/")[1].length == 5) "HTTP/1.1 200 OK\r\n\r\n" else "HTTP/1.1 404 Not Found\\r\\n\\r\\n"
     output.println(response)
 }
